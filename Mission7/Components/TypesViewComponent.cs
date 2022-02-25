29788bd6cd3034fca9894541faa16a5f15583d53
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-02-24 22:04:01 -0700

    Initial commit

---
+++ Mission7/Components/TypesViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+
+namespace Mission7.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        private IBooksRepository repo { get; set; }
+
+        public TypesViewComponent (IBooksRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Selected = RouteData?.Values["bookCategory"];
+
+            var types = repo.Books.Select(x => x.Category).Distinct().OrderBy(x => x);
+
+            return View(types);
+        }
+    }
+}
---
+++ Mission7/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+using Mission7.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBooksRepository repo;
+
+        public HomeController(IBooksRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(string bookCategory, int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books.Where(x => x.Category == bookCategory || bookCategory == null).OrderBy(b => b.Title).Skip(pageSize * (pageNum - 1)).Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = (bookCategory == null ? repo.Books.Count() : repo.Books.Where(x => x.Category == bookCategory).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
